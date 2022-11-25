8ba037b05b8275705534916b2bd699d6466e5471
JorensM <jorensmerenjanu@gmail.com>
2022-11-25 14:09:00 +0200

    finished

--- elementor-image-alt-tags.php
+++ elementor-image-alt-tags.php
@@ -7,8 +7,51 @@
 
 function j_change_image_widget_content($widget_content, $widget){
     if($widget->get_name() === "image"){
-        echo "<pre>";
-        echo $widget_content;
-        echo "</pre>";
+
+        $j_settings = $widget->get_settings();
+        $alt_tag = null;
+
+        if(!empty($j_settings["image_alt_tag"])){
+            $alt_tag = $j_settings["image_alt_tag"];
+        }else{
+            return $widget_content;
+        }
+
+        $doc = new DOMDocument();
+        @$doc->loadHTML($widget_content);
+        $selector = new DOMXPath($doc);
+
+        $result = $selector->query("//img[@alt]");
+
+        $has_alt = false;
+
+        $new_widget_content = false;
+
+        if($result->count() > 0){
+
+            $result->item(0)->setAttribute("alt", $alt_tag);
+
+            $html_element = $selector->query("//body")->item(0)->childNodes->item(0);
+
+            $new_widget_content = $doc->saveHTML($html_element);
+            return $new_widget_content;
+        }
+
+        return $widget_content;
     }
 }
+
+function j_add_image_widget_settings($element, $section_id, $args){
+    if($element->get_name() === "image" && $section_id === "section_image"){
+        $element->add_control(
+			'image_alt_tag',
+			[
+				'type' => \Elementor\Controls_Manager::TEXTAREA,
+				'label' => esc_html__( 'Alt tag', 'j_widgets' ),
+			]
+		);
+    }
+}
+
+add_filter( 'elementor/widget/render_content', 'j_change_image_widget_content', 10, 2 );
+add_action( 'elementor/element/before_section_end', 'j_add_image_widget_settings', 10, 3 );
